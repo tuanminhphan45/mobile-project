a1f9b94bd1398800bb533e5092ae9f79cfb1b6ec
LongHoang77 <longh.772004@gmail.com>
2024-09-14 22:46:22 +0700

    update inbox to write

---
+++ app/src/main/java/vn/edu/usth/mobile_project/Activity/WriteActivity.java
@@ -0,0 +1,42 @@
+package vn.edu.usth.mobile_project.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.usth.mobile_project.R;
+
+public class WriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_write);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageButton button = findViewById(R.id.back_write);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WriteActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+}
